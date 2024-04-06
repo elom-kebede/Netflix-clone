313d2e329876574e5d65dd5b808b4b90f4fbd5cb
elom-kebede <kebedeelias30@gmail.com>
2024-04-06 06:19:00 -0800

    axios and request

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 
+
 function Home() {
   return (
     <>
       <Header/>
       <Footer/>
+      
     </>
   )
 }
