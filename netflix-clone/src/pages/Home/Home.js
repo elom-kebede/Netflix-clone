26c1a82406b3ed93d8f691cc32fe2949743bb481
elom-kebede <kebedeelias30@gmail.com>
2024-04-06 12:22:03 -0800

    Banner

--- netflix-clone/src/pages/Home/Home.js
+++ netflix-clone/src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import Banner from '../../components/banner/Banner'
 
 
 function Home() {
   return (
     <>
       <Header/>
+      <Banner/>
       <Footer/>
       
     </>
